84e118b573fdcc2ed0bbafe0654b6ccef1337f31
Ersin KAYHAN <dekked2438@gmail.com>
2023-05-24 17:41:13 +0300

    Michael Day1

--- src/week1/Michael.java
+++ src/week1/Michael.java
@@ -3,6 +3,7 @@ package week1;
 public class Michael {
     public static void main(String[] args) {
         System.out.println("Hello everybody");
+        System.out.println("Great group Mehr");
 
     }
 }
