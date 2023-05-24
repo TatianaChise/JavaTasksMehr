832bb411cbdf3ea39e53ef21bfdda29737b98105
Rilas2023 <said.koudil@gmail.com>
2023-05-24 09:49:14 -0500

    Said Day1 updated

--- src/week1/Said.java
+++ src/week1/Said.java
@@ -2,7 +2,9 @@ package week1;
 
 public class Said {
     public static void main(String[] args) {
-
         System.out.println("Hi everyone");
+        System.out.println("Best Cydeo group: Mehr");
+
+
     }
 }
