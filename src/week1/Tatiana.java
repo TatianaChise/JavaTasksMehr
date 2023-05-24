f7ff032fb20a3374593920e11305fc94fc690240
tatianaocnovanu <ocnovanutanya@yahoo.com>
2023-05-24 10:10:43 -0500

    added day 2

--- src/week1/Tatiana.java
+++ src/week1/Tatiana.java
@@ -3,6 +3,9 @@ package week1;
 public class Tatiana {
     public static void main(String[] args) {
         System.out.println("Hi everyone");
+
+        System.out.println(" next try");
+
     }
 
 
