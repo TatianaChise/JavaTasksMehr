31bcd72eacc43eee38954eb8a0c577e567ed395c
Ersin KAYHAN <dekked2438@gmail.com>
2023-05-25 17:31:51 +0300

    Day1 of week2

---
+++ src/week2/Michael.java
@@ -0,0 +1,8 @@
+package week2;
+
+public class Michael {
+    public static void main(String[] args) {
+        System.out.println("Hi friends, we're gonna share our solution for the second week Java Tasks in this Package.");
+        System.out.println("Don't forget to push and update daily!!!");
+    }
+}
