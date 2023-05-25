f2d007e7d1ebcb39be78b55b1447115613605992
tatianaocnovanu <ocnovanutanya@yahoo.com>
2023-05-25 04:41:27 -0500

    Added first task solution for week 1.

--- src/week1/Tatiana.java
+++ src/week1/Tatiana.java
@@ -2,11 +2,26 @@ package week1;
 
 public class Tatiana {
     public static void main(String[] args) {
-        System.out.println("Hi everyone");
+        System.out.println("Good morning! Happy Thursday for everyone!");
 
-        System.out.println(" next try");
+        System.out.println("Week 1, has 3 Java tasks");
+        System.out.println("#1. Write a method which can identifies given number is even or odd.\nEX :\n\t identify(5)->'Odd'\n\t identify(6)->'Even'");
+
+        OddOrEven(5);
+        OddOrEven(6);
+
 
     }
 
-
+    public static void OddOrEven(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number");
+        } else {
+            System.out.println(number + " is an odd number");
+        }
+    }
 }
+
+
+
+
