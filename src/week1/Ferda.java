0abd34778ce7612a23730b42f1cbe84426313427
Ferda Kizilay <ferda.kizilay@yahoo.com>
2023-05-25 22:37:39 -0500

    Task 2 added

--- src/week1/Ferda.java
+++ src/week1/Ferda.java
@@ -3,15 +3,24 @@ package week1;
 public class Ferda {
 
     public static void main(String[] args) {
-        System.out.println("Hi everyone, how is it going?");
-        System.out.println("again me");
-        int a=20;
+
         System.out.println(oddOrEven(20));
+        System.out.println(division(2,20));
     }
     public static String oddOrEven(int a){
         if(a%2==0){
             return "Even";
         }
         return "Odd";
+    }
+
+    public static int division(int a, int b){
+        int count=0;
+        for(int i=a; i<=b; i=i+a){
+            if(a<=b){
+                count=count+1;
+            }
+        }
+        return count;
     }
 }
