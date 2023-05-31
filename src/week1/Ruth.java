b2f079fedd6798db10e9eef3c1e689be8bc0997a
Ruth <berakiruth@gmail.com>
2023-05-31 16:27:47 -0500

    Ruth.java for week 1 and week 2 updated

--- src/week1/Ruth.java
+++ src/week1/Ruth.java
@@ -1,4 +1,31 @@
 package week1;
 
 public class Ruth {
+    public static int divide(int x,int y){
+        int quotient=0;
+
+        for (int i = y; i <=x ; ) {
+            x=x-y;
+            quotient++;
+        }
+        return quotient;
+
+    }
+
+    ////////////////////////////////////////
+    public static String finra(int num){
+        // boolean div3=(num%3==0); boolean div5=(num%5==0);
+        String str="";
+        for (int i = 1; i <= num; i++) {
+            boolean div3=(i%3==0); boolean div5=(i%5==0);
+            str+=i+" "+((div3&&div5)?"FINRA ":div3?"FIN ":div5?"RA ":"");
+        }
+
+        return str;
+    }
+    //////////////////////////////////////////////
+    public static void main(String[] args) {
+        System.out.println(finra(50));
+        System.out.println(divide(10,3));
+    }
 }
---
+++ src/week2/Ruth.java
@@ -0,0 +1,69 @@
+package week2;
+
+import java.util.ArrayList;
+
+public class Ruth {
+    public static String  swap(int x, int y)
+    {
+        String str="";
+
+        x=x*y;
+        y=x/y;
+        x=x/y;
+        str+="x="+x+" y="+y;
+        return str;
+
+    }
+
+    public static String consecutiveNumbers(int x){
+        String str="";
+        for (int i = 1; i <=x ; i++) {
+            if ((i%2!=0)&&(i%3!=0)&&(i%5!=0)){str+=""+i;}
+            if (i%2==0){str+="Codility";}
+            if (i%3==0){str+="Test";}
+            if (i%5==0){str+="Coders";}
+            str+="\n";
+
+        }
+
+        return str;
+
+    }
+    public static void divlist(int x){
+        ArrayList<Integer> div3 = new ArrayList<>();
+        ArrayList<Integer>div5=new ArrayList<>();
+        ArrayList<Integer>div15=new ArrayList<>();
+        for (int i=1; i<=x; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0) {div15.add(i);}
+            if (i % 3 == 0){div3.add(i);}
+            if (i % 5 == 0){div5.add(i);}
+
+        }
+        System.out.println("divisible by 15 "+div15);
+        System.out.println("divisible by  5 "+div5);
+        System.out.println("divisible by  3 "+div3);
+    }
+public static String divisibleby3515(int x){
+        String str="";
+
+        String div3="Divisible by 3: ";
+        String div5="Divisible by 5: ";
+        String div15="Divisible by 15: ";
+        for (int i=1; i<=x; i++)
+        {
+            if(i % 3 == 0 && i % 5 == 0) {div15+=" "+i;}
+            if (i % 3 == 0){div3+=" "+i;}
+            if (i % 5 == 0){div5+=" "+i;}
+
+            }
+str=div3+"\n"+div5+"\n"+div15;
+        return str;
+}
+    public static void main(String[] args) {
+        System.out.println(consecutiveNumbers(30));
+        System.out.println(swap(3,8));
+        System.out.println(divisibleby3515(60));
+        divlist(60);
+    }
+}
