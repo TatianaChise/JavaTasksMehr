ec5f85a268d3f23efbac1a3d07d516f8c868187c
Tatiana_Chise <ocnovanutanya@yahoo.com>
2023-06-09 06:30:48 -0500

    created a week 4 class

--- src/week4/Tatiana.java
+++ src/week4/Tatiana.java
@@ -1,4 +1,16 @@
 package week4;
 
 public class Tatiana {
+    public static void main(String[] args) {
+        System.out.println("Week 4,has 3 Tasks");
+        System.out.println("\t #1. Task: Frequency of Characters\n\tWrite a return method that can find the frequency of characters:  \n\tEX: FrequencyOfCharacters(AAABBCDD)==>A3B2C1D2");
+
+        System.out.println("\t #2. Task: Same letters\n\tWrite a return method that check if a string is build out of yje same letters as another string.\n\tEX:same(abc,cab);-->true\n\tsame(abc,abb);-->false");
+
+        System.out.println("\t #3 Task: Remove Duplicates\n\tWrite a method that can remove the duplicated values from String\n\tEX: removeDup(AAABBBCCC)==>ABC");
+
+
+
+
+    }
 }
