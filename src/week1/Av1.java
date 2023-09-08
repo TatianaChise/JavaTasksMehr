908311c99a442fe39bf0247a8154a40055a2db53
Alex <alexvinnyk@yahoo.com>
2023-09-07 23:34:24 -0500

    Added commit.commit added.

---
+++ src/week1/Av1.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Av1 {
+    public static void main(String[] args) {
+
+
+    int y=5;
+    int x=4;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+
+   x = x + y;
+   y = x - y;
+   x = x - y;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+    }
+}
